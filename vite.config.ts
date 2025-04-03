52a1f3fb34d4f1d98e03a3dd66b141485ba495ef
mentaldistressed <convulsions@internet.ru>
2025-04-03 14:36:38 +0300

    asdasd

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { componentTagger } from "lovable-tagger";
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
-    allowedHosts: ['rpluslb.ru'],
+    allowedHosts: ['rpluslb.ru', '0f31f3a0-944b-401d-a322-8385570dd2c3.lovableproject.com', 'https://where.d1.test.destinations.yandex2go.cookie.kitchen-helper.fye.rpluslb.ru/'],
     port: 8080,
   },
   plugins: [
